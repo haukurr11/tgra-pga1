7bbb086c5df4911d3d8b22a41c846c440e57adde
Aron <bigron86@gmail.com>
2013-09-17 16:03:37 +0000

    ScreenText tilbuid, birtir,addar og resettar.

--- src/is/ru/tgra/ScreenText.java
+++ src/is/ru/tgra/ScreenText.java
@@ -1,55 +1,40 @@
 package is.ru.tgra;
 
-
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class ScreenText{
-  private String text;
-  private Vector2 position;
-  private int width;
-  private int height;
   private int x;
   private int y;
+  private SpriteBatch spriteBatch;
+  private BitmapFont font;
+  private String scoreDisplay;
+  private int score;
 
-  public ScreenText(String text,int x,int y){
-      this.text = text;
-      this.position = position;
-      this.x=x;
-      this.y=y;
+  public ScreenText(){
+      this.spriteBatch = new SpriteBatch();
+      this.font = new BitmapFont();
+      this.score = 0;
+      this.scoreDisplay = "score: " + score;
   }
 
-  public void draw(SpriteBatch sp,BitmapFont fnt){
-      width=(int)fnt.getBounds(text).width; //Get the width of the text we draw using the current font
-      height=(int)fnt.getBounds(text).height; //Get the height of the text we draw using the current font
-      fnt.draw(sp,text,this.x-width/2, // Get center value in x direction
-              this.y+height/2 //Get center value in y direction
-              );
+  public void drawScore(){
+	  spriteBatch.begin(); 
+	  font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+	  font.draw(spriteBatch, scoreDisplay, 25, 100); 
+	  spriteBatch.end();
   }
-
-  public String getText() {
-      return text;
+  
+  public void addScore(int adding){
+	  this.score += adding;
+	  this.scoreDisplay = "score:" + score;
   }
-
-  public void setText(String text) {
-      this.text = text;
+  
+  public void resetScore(){
+	  this.score = 0;
+	  this.scoreDisplay = "score:" + score;
   }
-
-  public Vector2 getPosition() {
-      return position;
-  }
-
-  public void setPosition(Vector2 position) {
-      this.position = position;
-  }
-
-  public int getWidth() {
-      return width;
-  }
-
-  public int getHeight() {
-      return height;
-  }
-
+  
 }
